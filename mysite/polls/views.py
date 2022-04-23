5657d3b76663270b66a697962c54e99bc37c250a
AlexS778 <aleksej.serbinov29@gmail.com>
2022-04-23 21:16:39 +0300

    fixed templates etc

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render,redirect
-
-
+from django.contrib import messages
 from django.contrib.auth import login
+from django.shortcuts import redirect, render
+from django.views.generic import DetailView, ListView
+
 from .forms import NewUserForm
 from .models import Maker, Tweet
-from django.views.generic import DetailView, ListView
-from django.contrib import messages
+
 # Create your views here.
 
 # def home(request):
@@ -13,33 +13,35 @@ from django.contrib import messages
 #     context = {'name':name}
 #     return render(request,'home.html',context)
 
+
 class ForumListDetail(ListView):
-	model = Maker
-	template_name = "home.html"
-	
-	def get(self, request):
-		name = Maker.objects.all()
-		ctx = {"name": name}
-		return render(request, self.template_name, ctx)
+    model = Maker
+    template_name = "home.html"
+
+    def get(self, request):
+        name = Maker.objects.all()
+        ctx = {"name": name}
+        return render(request, self.template_name, ctx)
 
 
 def register_request(request):
-	if request.method == "POST":
-		form = NewUserForm(request.POST)
-		if form.is_valid():
-			user = form.save()
-			login(request, user)
-			messages.success(request, "Registration successful." )
-			return redirect(home)
-		messages.error(request, "Unsuccessful registration. Invalid information.")
-	form = NewUserForm()
-	return render (request,"signup.html", context={"register_form":form})
+    if request.method == "POST":
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Registration successful.")
+            return redirect(home)
+        messages.error(request, "Unsuccessful registration. Invalid information.")
+    form = NewUserForm()
+    return render(request, "signup.html", context={"register_form": form})
 
 
 class ForumDetailView(DetailView):
-	model = Tweet
-	template_name = "profile.html"
-	def get(self, request, pk):
-		user = Tweet.objects.get(id=pk)
-		context = {'user':user}
-		return render(request,self.template_name,context)
+    model = Tweet
+    template_name = "detail.html"
+
+    def get(self, request, pk):
+        user = Tweet.objects.get(id=pk)
+        context = {"tweet": user}
+        return render(request, self.template_name, context)
